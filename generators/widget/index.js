9b344aa9db19726cb1047ebd1a8fc14a43976fe0
Charles Bourasseau <charles.bourasseau@gmail.com>
2020-10-17 15:38:28 +0200

    Add `type` and `name` command line arguments

--- generators/widget/index.js
+++ generators/widget/index.js
@@ -4,6 +4,12 @@ const gulpPrettier = require("gulp-prettier");
 
 module.exports = class extends Generator {
   start() {
+    if (this.options.name && this._validate(this.options.name) === true) {
+      this.log(this.options.name);
+      this._generateFiles(this.options.name);
+      return ;
+    }
+
     this.prompt({
       type: "input",
       name: "widgetClassName",
@@ -11,41 +17,48 @@ module.exports = class extends Generator {
       filter(input) {
         return input.trim();
       },
-      validate(input) {
-        if (input.length === 0) {
-          return "A name for the widget class must be provided";
-        }
-        if (input.charAt(0) !== input.charAt(0).toUpperCase()) {
-          return "The name must start with a capital letter";
-        }
-        if (input.match(/\s/)) {
-          return "The name must not contain spaces";
-        }
-        return true;
+      validate: (input) => {
+        return this._validate(input);
       },
     }).then(answers => {
-      const widgetClassName = answers.widgetClassName;
-      const folder = `src/Widgets/${widgetClassName}`;
-      const humanFriendlyName = lodash.startCase(
+      this._generateFiles(answers.widgetClassName);
+    });
+  }
+
+  _validate(input) {
+    if (input.length === 0) {
+      return "A name for the widget class must be provided";
+    }
+    if (input.charAt(0) !== input.charAt(0).toUpperCase()) {
+      return "The name must start with a capital letter";
+    }
+    if (input.match(/\s/)) {
+      return "The name must not contain spaces";
+    }
+    return true;
+  }
+
+  _generateFiles(widgetClassName) {
+    const folder = `src/Widgets/${widgetClassName}`;
+    const humanFriendlyName = lodash.startCase(
         widgetClassName.replace(/Widget$/, "")
-      );
-      this._generateFile(
+    );
+    this._generateFile(
         "XWidgetClass.js.ejs",
         `${folder}/${widgetClassName}Class.js`,
         { widgetClassName }
-      );
-      this._generateFile(
+    );
+    this._generateFile(
         "XWidgetEditingConfig.js.ejs",
         `${folder}/${widgetClassName}EditingConfig.js`,
         { widgetClassName, humanFriendlyName }
-      );
-      this._generateFile(
+    );
+    this._generateFile(
         "XWidgetComponent.js.ejs",
         `${folder}/${widgetClassName}Component.js`,
         { widgetClassName }
-      );
-      this.registerTransformStream(gulpPrettier());
-    });
+    );
+    this.registerTransformStream(gulpPrettier());
   }
 
   _generateFile(templatePath, destinationPath, context) {
